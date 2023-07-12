0aab5313d0e6c460e64195d392083919bdbbcad3
keye2004cn <139268451+keye2004cn@users.noreply.github.com>
2023-07-12 10:49:10 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,4 +2,4 @@ import streamlit;
 streamlit.title('my parents healthy dinner')
 streamlit.header('breakfast menu')
 streamlit.text('Omega 3 & blueburry oatmeals')
-
+streamlit.text('Kale,spinach & Rocket Smoothies')
