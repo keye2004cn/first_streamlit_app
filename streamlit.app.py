498c0a43708824565b68a365c8037b0be31a6063
keye2004cn <139268451+keye2004cn@users.noreply.github.com>
2023-07-14 11:16:19 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -19,7 +19,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit"+"kiwi")
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
